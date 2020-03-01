91659ccabeffc07c809bbce05b64dfa41a8166aa
hwajj <59442897+hwajj@users.noreply.github.com>
2020-03-02 08:13:30 +0900

    Update py4e_dict.py
    문장을 입력하면 
    {'코끼리':2 ,'거미줄에':2 ,'신나게':1 ,'탔다네':1}
    문장안에 든 단어 쪼개서 개수 세어줌

--- py4e_dict.py
+++ py4e_dict.py
@@ -1,7 +1,7 @@
 count=dict()
-print('Enter a line of text:')
+print('문장을 입력하시면 문장안에 단어 분포를 분석해 드려요:')
 line=input('')
 words=line.split()
 for word in words:
     count[word]=count.get(word,0)+1
-print('count', count)
+print(count)
